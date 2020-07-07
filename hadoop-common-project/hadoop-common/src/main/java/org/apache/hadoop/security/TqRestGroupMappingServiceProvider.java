43bac98e72cf5d83fc623e36f1feda3f8fa7f615
daniedeng <daniedeng@tencent.com>
2020-07-07 15:49:24 +0800

    [THADOOP-28] Support tq authentication 3/3

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/TqRestGroupMappingServiceProvider.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/TqRestGroupMappingServiceProvider.java
@@ -1,9 +1,6 @@
 package org.apache.hadoop.security;
 
-import com.tencent.tdw.security.authentication.service.SecureService;
-import com.tencent.tdw.security.authentication.service.SecureServiceFactory;
-import org.apache.hadoop.conf.Configurable;
-import org.apache.hadoop.conf.Configuration;
+import com.tencent.tdw.security.utils.RestUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,19 +8,18 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-public class TqRestGroupMappingServiceProvider implements GroupMappingServiceProvider, Configurable {
+public class TqRestGroupMappingServiceProvider implements GroupMappingServiceProvider {
   private static final Logger LOG = LoggerFactory.getLogger(TqRestGroupMappingServiceProvider.class);
-  private Configuration conf;
-  private SecureService secureService;
   private GroupMappingServiceProvider fallback = new ShellBasedUnixGroupsMapping();
 
   @Override
   public List<String> getGroups(String user) throws IOException {
     try {
-      if (secureService != null) {
-        return secureService.getGroups(user);
-      }
-
+      return RestUtil.getGroups(user);
+    } catch (Exception e) {
+      LOG.warn("Failed to get group for " + user + ", by " + e.getMessage());
+    }
+    try {
       return fallback.getGroups(user);
     } catch (Exception e) {
       LOG.warn("Failed to get group for " + user + ", by " + e.getMessage());
@@ -39,20 +35,5 @@ public class TqRestGroupMappingServiceProvider implements GroupMappingServicePro
   @Override
   public void cacheGroupsAdd(List<String> groups) throws IOException {
 
-  }
-
-  @Override
-  public void setConf(Configuration conf) {
-    this.conf = conf;
-    try {
-      secureService = SecureServiceFactory.getDefault();
-    } catch (Exception e) {
-      LOG.warn("Initialization failed", e);
-    }
-  }
-
-  @Override
-  public Configuration getConf() {
-    return conf;
   }
 }
--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/token/delegation/AbstractDelegationTokenSelector.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/token/delegation/AbstractDelegationTokenSelector.java
@@ -18,14 +18,21 @@
 
 package org.apache.hadoop.security.token.delegation;
 
+import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 
+import com.tencent.tdw.security.utils.StringUtils;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.security.Utils;
 import org.apache.hadoop.security.token.Token;
 import org.apache.hadoop.security.token.TokenIdentifier;
 import org.apache.hadoop.security.token.TokenSelector;
+import org.apache.hadoop.security.token.TqTokenIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Look through tokens to find the first delegation token that matches the
@@ -37,23 +44,87 @@ public
 class AbstractDelegationTokenSelector<TokenIdent 
 extends AbstractDelegationTokenIdentifier> 
     implements TokenSelector<TokenIdent> {
+  private static final Logger LOG = LoggerFactory.getLogger(AbstractDelegationTokenSelector.class);
   private Text kindName;
+  private boolean unionTokenEnable;
+  private boolean tqTokenEnable;
+  private boolean filterEnable;
   
   protected AbstractDelegationTokenSelector(Text kindName) {
     this.kindName = kindName;
+    this.unionTokenEnable = Utils.getSystemPropertyOrEnvVar(
+        String.format("tq.%s.union.token.enable", kindName.toString()), true);
+    this.tqTokenEnable = Utils.getSystemPropertyOrEnvVar(
+        String.format("tq.%s.token.enable", kindName.toString()), true);
+    this.filterEnable = Utils.getSystemPropertyOrEnvVar(
+        String.format("tq.%s.token.filter.enable", kindName.toString()), true);
   }
 
   @SuppressWarnings("unchecked")
   @Override
   public Token<TokenIdent> selectToken(Text service,
       Collection<Token<? extends TokenIdentifier>> tokens) {
-    if (service == null) {
+    if (service == null || tokens == null || tokens.isEmpty()) {
       return null;
     }
     for (Token<? extends TokenIdentifier> token : tokens) {
       if (kindName.equals(token.getKind())
           && service.equals(token.getService())) {
         return (Token<TokenIdent>) token;
+      }
+    }
+    // not found corresponding token, try found union token
+    if (unionTokenEnable) {
+      for (Token<? extends TokenIdentifier> token : tokens) {
+        if (kindName.equals(token.getKind())) {
+          if (AbstractDelegationTokenIdentifier.getVersion(token.getIdentifier())
+              == AbstractDelegationTokenIdentifier.UNION_DELEGATION_TOKEN_IDENTIFIER_VERSION) {
+            TokenIdent tokenIdent;
+            try {
+              tokenIdent = (TokenIdent) token.decodeIdentifier();
+            } catch (IOException e) {
+              LOG.warn("Failed to decode identifier : " + token, e);
+              continue;
+            }
+            if (tokenIdent != null && tokenIdent.isUnion()) {
+              if (LOG.isDebugEnabled()) {
+                LOG.debug("Use Union token for " + service + " ：" + token);
+              }
+              return (Token<TokenIdent>) token;
+            }
+          }
+        }
+      }
+    }
+    //try found from tq token
+    if (tqTokenEnable) {
+      for (Token<? extends TokenIdentifier> token : tokens) {
+        if (token.getKind().equals(TqTokenIdentifier.TQ_TOKEN)) {
+          boolean filter = true;
+          if (filterEnable) {
+            try {
+              TqTokenIdentifier tokenIdentifier = (TqTokenIdentifier) token.decodeIdentifier();
+              String ignoreStr = tokenIdentifier.getProps().get("ignore");
+              if (!StringUtils.isBlank(ignoreStr)) {
+                List<String> ignoreList = StringUtils.getStringCollection(ignoreStr);
+                for (String ignore : ignoreList) {
+                  if (service.toString().startsWith(ignore)) {
+                    filter = false;
+                  }
+                }
+              }
+            } catch (IOException e) {
+              LOG.warn("Failed to decode identifier : " + token, e);
+              continue;
+            }
+          }
+          if (filter) {
+            if (LOG.isDebugEnabled()) {
+              LOG.debug("Use tq token for " + service + " ：" + token);
+            }
+            return (Token<TokenIdent>) token;
+          }
+        }
       }
     }
     return null;
