a1125271617c60c4de5ce5f094d011a61d5e2181
xudongcao <xudongcao@tencent.com>
2020-12-17 20:21:00 +0800

    THADOOP-107. Active NN in ConfiguredFailoverProxyProvider should be presented as a String in active cache file. (merge request !257)

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/server/namenode/ha/ConfiguredFailoverProxyProvider.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/server/namenode/ha/ConfiguredFailoverProxyProvider.java
@@ -63,7 +63,7 @@ public class ConfiguredFailoverProxyProvider<T> extends
         HdfsClientConfigKeys.Failover.CACHE_ACTIVE_DIR_KEY,
         HdfsClientConfigKeys.Failover.CACHE_ACTIVE_DIR_DEFAULT);
     this.cacheActiveFile = new File(cacheActiveDir, uri.getHost());
-    this.currentProxyIndex = readActiveCache();
+    this.currentProxyIndex = getActiveIndex();
   }
 
   /**
@@ -78,7 +78,7 @@ public class ConfiguredFailoverProxyProvider<T> extends
   @Override
   public  void performFailover(T currentProxy) {
     incrementProxyIndex();
-    writeActiveCache(currentProxyIndex);
+    writeActiveCache();
   }
 
   synchronized void incrementProxyIndex() {
@@ -110,14 +110,16 @@ public class ConfiguredFailoverProxyProvider<T> extends
     return true;
   }
 
-
   /**
-   * Write active NameNode index to cache file.
+   * Write active NameNode to cache file.
    */
-  private void writeActiveCache(int index) {
+  private void writeActiveCache() {
     if (!cacheActiveEnabled) {
       return;
     }
+
+    String currentActive =
+        proxies.get(currentProxyIndex).getAddress().getHostString();
 
     synchronized (cacheActiveFile) {
       try (RandomAccessFile raf = new RandomAccessFile(cacheActiveFile, "rw");
@@ -125,33 +127,35 @@ public class ConfiguredFailoverProxyProvider<T> extends
           FileLock lock = fc.tryLock(0, Long.MAX_VALUE, false)) {
         if (lock != null) {
           raf.setLength(0);
-          raf.writeBytes(String.valueOf(index));
+          raf.writeBytes(currentActive);
           boolean ret = cacheActiveFile.setWritable(true, false)
                         && cacheActiveFile.setReadable(true, false);
           if (!ret) {
             throw new IOException("Cannot set file rw mode.");
           }
-          LOG.debug("Succeed in writing active index " + index
-              + " to cache file " + cacheActiveFile);
+          LOG.debug("Succeed in writing current active NameNode "
+              + currentActive + " to cache file " + cacheActiveFile);
         }
       } catch (Throwable e) {
-        LOG.warn("Failed to write active index " + index + " to cache file "
-            + cacheActiveFile + ", It may be due to permission issues, the "
-            + "dfs.client.failover.cache-active.dir must be properly "
-            + "configured!", e);
+        LOG.warn("Failed to write current active NameNode " + currentActive
+            + " to cache file " + cacheActiveFile + ", It may be due to "
+            + "permission issues, the dfs.client.failover.cache-active.dir "
+            + "must be properly configured!", e);
       }
     }
   }
 
   /**
-   * Read active NameNode index from cache file.
+   * Read active NameNode from cache file.
    */
-  private int readActiveCache() {
+  private int getActiveIndex() {
     if (!cacheActiveEnabled) {
       return 0;
     }
 
-    int index = 0;
+    String currentActive = null;
+    int activeindex = 0;
+
     synchronized (cacheActiveFile) {
       if (!cacheActiveFile.exists()) {
         return 0;
@@ -161,19 +165,23 @@ public class ConfiguredFailoverProxyProvider<T> extends
           FileChannel fc = raf.getChannel();
           FileLock lock = fc.tryLock(0, Long.MAX_VALUE, true)) {
         if (lock != null) {
-          index = Integer.parseInt(raf.readLine()) % proxies.size();
-          // sanity check
-          if (index < 0) {
-            return 0;
-          }
+          currentActive = raf.readLine();
         }
       } catch (Throwable e) {
-        LOG.warn("Failed to read active index from cache file "
+        LOG.warn("Failed to read active NameNode from cache file "
             + cacheActiveFile + ", It may be due to permission issues, the "
             + "dfs.client.failover.cache-active.dir must be properly "
             + "configured! Now we will begin from index 0.", e);
+        return 0;
       }
     }
-    return index;
+
+    for (int i = 0; i < proxies.size(); i++) {
+      if (proxies.get(i).getAddress().getHostString().equals(currentActive)) {
+        activeindex = i;
+        break;
+      }
+    }
+    return activeindex;
   }
 }
